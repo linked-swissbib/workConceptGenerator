3ada36854fd94eab8ddb1914abbc15e7e7871a62
sschuepbach <sebastian.schuepbach@unibas.ch>
2016-04-19 06:47:37 +0200

    First commit

---
+++ build.sbt
@@ -0,0 +1,40 @@
+lazy val commonSettings = Seq(
+  organization := "ch.swissbib.linked",
+  version := "0.1",
+  scalaVersion := "2.10.6"
+)
+
+assemblyMergeStrategy in assembly := {
+  case PathList("javax", "servlet", xs@_*) => MergeStrategy.last
+  case PathList("javax", "activation", xs@_*) => MergeStrategy.last
+  case PathList("org", "apache", xs@_*) => MergeStrategy.last
+  case PathList("com", "google", xs@_*) => MergeStrategy.last
+  case PathList("com", "esotericsoftware", xs@_*) => MergeStrategy.last
+  case PathList("com", "codahale", xs@_*) => MergeStrategy.last
+  case PathList("com", "yammer", xs@_*) => MergeStrategy.last
+  case "about.html" => MergeStrategy.rename
+  case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+  case "META-INF/mailcap" => MergeStrategy.last
+  case "META-INF/mimetypes.default" => MergeStrategy.last
+  case "plugin.properties" => MergeStrategy.last
+  case "log4j.properties" => MergeStrategy.last
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
+
+lazy val sparkVersion = "1.6.1"
+
+lazy val root = (project in file("."))
+  .settings(commonSettings: _*)
+  .settings(
+
+    name := "workConceptGenerator",
+
+    resolvers += "clojars" at "https://clojars.org/repo",
+    resolvers += "conjars" at "http://conjars.org/repo",
+    libraryDependencies ++= Seq(
+      "org.apache.spark" %% "spark-core" % sparkVersion exclude ("org.scala-lang", "*"),
+      "org.elasticsearch" %% "elasticsearch-spark" % "2.3.0"
+    )
+  )
---
+++ src/main/scala-2.10/ch/swissbib/linked/App.scala
@@ -0,0 +1,73 @@
+package ch.swissbib.linked
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.elasticsearch.spark._
+import org.elasticsearch.spark.rdd.Metadata._
+
+import scala.collection.mutable
+
+/**
+  * @author Sebastian Schüpbach
+  * @version 0.1
+  *
+  *          Created on 25.03.16
+  */
+object App {
+
+  def main(args: Array[String]): Unit = {
+    val master = "local[8]"
+    val appName = "Work Concept Generator"
+    val sparkHome = "/usr/local/spark"
+    val esNodes = "localhost"
+    val esPort = "9200"
+    implicit val esIndex = "testsb_160407"
+    val esOriginType = "bibliographicResource"
+    val esTargetType = "work3"
+    def esIndexType(t: String)(implicit i: String) = i + "/" + t
+
+    val sparkConfig = new SparkConf()
+      .setMaster(master)
+      .setAppName(appName)
+      .setSparkHome(sparkHome)
+      .set("es.nodes", esNodes)
+      .set("es.port", esPort)
+      .set("es.mapping.date.rich", "false")
+
+    val valueCollector = (agg: mutable.Map[String, mutable.Buffer[String]], elem: Map[String, AnyRef]) => {
+      for (key <- elem.keys) {
+        elem(key) match {
+          case s: String => if (s != "") {
+            agg(key) ++= mutable.Buffer(s)
+            agg(key) = agg(key).distinct
+          }
+          case b: mutable.Buffer[String] =>
+            agg(key) ++= b.filter(_ != "")
+            agg(key) = agg(key).distinct
+          case _ => throw new Exception("Not supported!")
+        }
+      }
+      agg
+    }
+
+    new SparkContext(sparkConfig)
+      .esRDD(esIndexType(esOriginType), "?q=_exists_:work")
+      .flatMap(x => {
+        x._2.get("work").get match {
+          case a: Seq[AnyRef] => a.map(Tuple2(_, Map("bf:hasInstance" -> x._1, "dct:contributor" -> x._2.getOrElse("dct:contributor", ""), "dct:title" -> x._2.getOrElse("dct:title", ""))))
+          case s => Seq(Tuple2(s, Map("bf:hasInstance" -> x._1, "dct:contributor" -> x._2.getOrElse("dct:contributor", ""), "dct:title" -> x._2.getOrElse("dct:title", ""))))
+        }
+      }
+      )
+      .groupBy(_._1)
+      .map(_._2.toBuffer)
+      .map(e => {
+        if (e.length > 1) {
+          val v = e.foldLeft(mutable.Map("bf:hasInstance" -> mutable.Buffer[String](), "dct:contributor" -> mutable.Buffer[String](), "dct:title" -> mutable.Buffer[String]()))((x, y) => valueCollector(x, y._2))
+          Tuple2(Map(ID -> e.head._1), v)
+        } else {
+          Tuple2(Map(ID -> e.head._1), e.head._2)
+        }
+      })
+      .saveToEsWithMeta(esIndexType(esTargetType))
+  }
+}
